23f37634ab1a891bbe60e8acfba7336f9f242ebb
DELLSTORE <166585589+MAKERBKZZ@users.noreply.github.com>
2024-06-10 12:44:48 +0700

    Update ig.py

--- ig.py
+++ ig.py
@@ -78,7 +78,7 @@ class Adels:
                 'Content-Type': 'application/json'
             }
             data = {
-                "min_name_length": 7,
+                "min_name_length": 4,
                 "max_name_length": 10
             }
             try:
