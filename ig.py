824df4041a0c080c4763ef36d9080a505248049d
DELLSTORE <166585589+MAKERBKZZ@users.noreply.github.com>
2024-06-10 13:03:58 +0700

    Update ig.py

--- ig.py
+++ ig.py
@@ -7,22 +7,22 @@ from fake_useragent import UserAgent
 
 requests.packages.urllib3.disable_warnings()
 
-class Adels:
+class Fidra:
     def __init__(self):
         with open("username.txt", "r") as file:
             self.names = [line.strip() for line in file] 
         self.start_time = sleper.time()
         self.time = int(datetime.now().timestamp())
         self.numbers = '123456789'
-        self.chars = 'Dzz1qwertyuiopasdfghjklzxcvbnm1234567890'
+        self.chars = 'qwertyuiopasdfghjklzxcvbnm1234567890'
         self.length = random.randint(6, 8)
         self.created = 0
-        self.status = True
-        self.password = ''.join(random.choice(ascii_letters + digits) for _ in range(random.randint(7, 15)))
+        self.status = None
+        self.password = ''.join(random.choice(ascii_letters + digits) for _ in range(random.randint(8, 14)))
         self.app_id = str("".join(random.choice(self.numbers) for i in range(15)))
         self.year = random.randint(1990, 1999)
         self.month = random.randint(1, 12)
-        self.day = random.randint(1, 29)
+        self.day = random.randint(1, 20)
         self.ig_did = str(uuid4()).upper()
         self.ua = UserAgent()
 
@@ -55,7 +55,6 @@ class Adels:
             headers = {
                 'User-Agent': self.ua.random,
                 'X-IG-App-ID': f'{self.app_id}',
-                'Origin': 'https://www.instagram.com/',
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Cookie': f'csrftoken={self.csrftoken}; ig_did={self.ig_did}; ig_nrcb=1; mid={self.mid}',
                 'X-CSRFToken': f'{self.csrftoken}'
@@ -74,12 +73,12 @@ class Adels:
         if self.check_username():
             url = 'https://api.internal.temp-mail.io/api/v3/email/new'
             headers = {
-                'User-Agent': 'Temp%20Mail/30 CFNetwork/1220.1 Darwin/20.3.0',
+                'User-Agent': self.ua.random,
                 'Content-Type': 'application/json'
             }
             data = {
                 "min_name_length": 5,
-                "max_name_length": 8
+                "max_name_length": 7
             }
             try:
                 response = requests.post(url, headers=headers, json=data, verify=False)
@@ -103,7 +102,6 @@ class Adels:
                 'User-Agent': self.ua.random,
                 'X-IG-App-ID': f'{self.app_id}',
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Origin': 'https://www.instagram.com/',
                 'Cookie': f'csrftoken={self.csrftoken}; ig_did={self.ig_did}; ig_nrcb=1; mid={self.mid}',
                 'X-CSRFToken': f'{self.csrftoken}'
             }
@@ -123,9 +121,7 @@ class Adels:
             'X-IG-App-ID': f'{self.app_id}',
             'Content-Type': 'application/x-www-form-urlencoded',
             'Cookie': f'csrftoken={self.csrftoken}; ig_did={self.ig_did}; ig_nrcb=1; mid={self.mid}',
-            'X-CSRFToken': f'{self.csrftoken}',
-            'User-Agent': self.ua.random,
-            'Origin': 'https://www.instagram.com/'
+            'X-CSRFToken': f'{self.csrftoken}'
         }
         requests.post(url, headers=headers, data=data)
 
@@ -133,8 +129,8 @@ class Adels:
         url = f'https://i.instagram.com/api/v1/users/web_profile_info/?username={username}'
         headers = {
             'User-Agent': self.ua.random,
-            'X-IG-App-ID': f'{self.app_id}',
-            'Origin': 'https://www.instagram.com/'
+            'X-IG-App-ID': '1217981644879628',
+            'X-ASBD-ID': '198387'
         }
         response = requests.get(url, headers=headers)
         if 'id' in response.text:
@@ -143,9 +139,9 @@ class Adels:
             return 'Suspend'
 
     def save_info(self, account, session):
-        with open('Adels-accounts.txt', 'a') as f:
+        with open('fidra-accounts.txt', 'a') as f:
             f.write(f'{account}\n')
-        with open('Adels-sessions.txt', 'a') as s:
+        with open('fidra-sessions.txt', 'a') as s:
             s.write(f'session:{session}\n')
 
     def send_code(self):
@@ -155,8 +151,6 @@ class Adels:
             headers = {
                 'User-Agent': self.ua.random,
                 'X-IG-App-ID': f'{self.app_id}',
-                'x-requested-with': 'XMLHttpRequest',
-                'Origin': 'https://www.instagram.com/',
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Cookie': f'csrftoken={self.csrftoken}; ig_did={self.ig_did}; ig_nrcb=1; mid={self.mid}',
                 'X-CSRFToken': f'{self.csrftoken}'
@@ -175,7 +169,7 @@ class Adels:
         if self.send_code():
             url = f'https://api.internal.temp-mail.io/api/v3/email/{self.email}/messages'
             headers = {
-                'User-Agent': 'Temp%20Mail/30 CFNetwork/1220.1 Darwin/20.3.0',
+                'User-Agent': self.ua.random,
                 'Content-Type': 'application/json'
             }
             while True:
@@ -203,10 +197,8 @@ class Adels:
                 'User-Agent': self.ua.random,
                 'X-IG-App-ID': f'{self.app_id}',
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Origin': 'https://www.instagram.com/',
                 'Cookie': f'csrftoken={self.csrftoken}; ig_did={self.ig_did}; ig_nrcb=1; mid={self.mid}',
-                'X-CSRFToken': f'{self.csrftoken}',
-
+                'X-CSRFToken': f'{self.csrftoken}'
             }
             response = requests.post(url, headers=headers, data=data)
             if 'signup_code' in response.text:
@@ -221,14 +213,13 @@ class Adels:
             print('[6] Creating Account')
             url = 'https://www.instagram.com/accounts/web_create_ajax/'
             time = int(datetime.now().timestamp())
-            data = f'enc_password=#PWD_INSTAGRAM_BROWSER:0:{time}:{self.password}&email={self.email}&username={self.username}&first_name=Created By Adels\n&month={self.month}&day={self.day}&year={self.year}&client_id=&seamless_login_enabled=1&tos_version=row&force_sign_up_code={self.signup_code}'
+            data = f'enc_password=#PWD_INSTAGRAM_BROWSER:0:{time}:{self.password}&email={self.email}&username={self.username}&first_name=Created By Fidra\n&month={self.month}&day={self.day}&year={self.year}&client_id=&seamless_login_enabled=1&tos_version=row&force_sign_up_code={self.signup_code}'
             headers = {
                 'User-Agent': self.ua.random,
                 'X-IG-App-ID': f'{self.app_id}',
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Cookie': f'csrftoken={self.csrftoken}; ig_did={self.ig_did}; ig_nrcb=1; mid={self.mid}',
-                'X-CSRFToken': f'{self.csrftoken}',
-                'Origin': 'https://www.instagram.com/'
+                'X-CSRFToken': f'{self.csrftoken}'
             }
             response = requests.post(url, headers=headers, data=data)
             if 'user_id' in response.text:
@@ -241,7 +232,7 @@ class Adels:
                 print(f'email : {self.email}')
                 print(f'Account Status : {self.account_status(self.username)}')
                 print(f'The process took {self.elapsed_time} seconds')
-                print('Account saved in "Adels-accounts.txt"')
+                print('Account saved in "fidra-accounts.txt"')
                 print('-' * 40)
                 session = response.cookies['sessionid']
                 account = f'{self.username}:{self.password}'
@@ -249,8 +240,6 @@ class Adels:
                 self.save_info(account, session)
                 self.send_to_bot(message)
             else:
-                if '"The IP address you are using has been flagged as an open proxy."' in response.text:
-                    response = '{"account_created": true, "status": "ok"}'
                 print('-' * 40)
                 print(response.text)
                 print('-' * 40)
@@ -270,10 +259,10 @@ Instagram Accounts Creator v1.0
 Powered By @f09l
 """)
 
-Adels = Adels()
+fidra = Fidra()
 try:
     count = int(input('accounts count : '))
 except:
-    count = 1
+    count = 0
 for _ in range(count):
-    Adels.create_account()
+    fidra.create_account()
